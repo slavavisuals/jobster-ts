6293fbdad790cd277de61f1fe0cb7f7ff9f4e48b
slava <slavavisuals@gmail.com>
2025-01-01 21:16:10 -0700

    initial comit

---
+++ components/JobInfo.tsx
@@ -0,0 +1,9 @@
+const JobInfo = ({ icon, text }: { icon: React.ReactNode; text: string }) => {
+  return (
+    <div className='flex gap-x-2 items-center'>
+      {icon}
+      {text}
+    </div>
+  );
+};
+export default JobInfo;
